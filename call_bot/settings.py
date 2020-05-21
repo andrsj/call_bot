2f88eca101e4d70b555537c00e907c22240ef14a
andrsj <andrsj.derkach@gmail.com>
2020-05-21 17:53:12 +0300

    Grouping setting in package

--- call_bot/settings.py
+++ call_bot/settings.py
@@ -1,19 +1,10 @@
-# SQL_URL = 'sqlite:///callbot.db'
-from configparser import ConfigParser
-config = ConfigParser()
-config.read('postgres.env')
-
-POSTGRES_USER = config.get('POSTGRES', 'POSTGRES_USER')
-POSTGRES_DB = config.get('POSTGRES', 'POSTGRES_DB')
-POSTGRES_PASSWORD = config.get('POSTGRES', 'POSTGRES_PASSWORD')
-POSTGRES_PORT = config.get('POSTGRES', 'POSTGRES_PORT')
-POSTGRES_HOST = config.get('POSTGRES', 'POSTGRES_HOST')
+from call_bot.settings.postgresql import *
 
 SQL_URL = f'postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}'
 
 
 try:
-    from call_bot.local_settings import *
+    from call_bot.settings.bot import *
 except Exception as e:
     print(e)
 
