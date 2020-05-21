ac258c41470a80a89cb668858a1c9b500aa45bd5
andrsj <andrsj.derkach@gmail.com>
2020-05-21 17:13:05 +0300

    Move DB to postgresql

--- call_bot/settings.py
+++ call_bot/settings.py
@@ -1,4 +1,15 @@
-SQL_URL = 'sqlite:///callbot.db'
+# SQL_URL = 'sqlite:///callbot.db'
+from configparser import ConfigParser
+config = ConfigParser()
+config.read('postgres.env')
+
+POSTGRES_USER = config.get('POSTGRES', 'POSTGRES_USER')
+POSTGRES_DB = config.get('POSTGRES', 'POSTGRES_DB')
+POSTGRES_PASSWORD = config.get('POSTGRES', 'POSTGRES_PASSWORD')
+POSTGRES_PORT = config.get('POSTGRES', 'POSTGRES_PORT')
+POSTGRES_HOST = config.get('POSTGRES', 'POSTGRES_HOST')
+
+SQL_URL = f'postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}'
 
 
 try:
