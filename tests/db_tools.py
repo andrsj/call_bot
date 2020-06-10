4c9bf9c5bbcad7ae298e1e7f2f947e47354bf0e7
Andrew Derkach <andrsj.derkach@gmail.com>
2020-06-10 08:24:17 +0000

    Remove prints and comment on_message

--- tests/db_tools.py
+++ tests/db_tools.py
@@ -6,7 +6,7 @@ from call_bot.models.phones import Phone
 from call_bot.settings import SQL_URL
 
 
-engine = create_engine(SQL_URL, echo=True)
+engine = create_engine(SQL_URL)
 Session = sessionmaker()
 Session.configure(bind=engine)
 session = Session()
@@ -25,12 +25,6 @@ def get_all_test_phones_str():
 def delete_test_users_by_name_like_test_():
     phones = session.query(Phone).filter(Phone.name.like('test_user%'))
     if phones.scalar():
-        print('\nExist phones with test_users:\n')
         for phone in phones:
-            print(f'Find: {phone}')
             session.delete(phone)
-            print(f'\nDelete: {phone}\n')
         session.commit()
-        print('\nCommited')
-    else:
-        print("\nNot found test_phones")
